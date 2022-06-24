a850e784bcb0a0b2d35330fc683ba4b27932ede0
wsydn <wsydney@web.de>
2022-06-24 16:04:30 +0200

    Layout improvements

--- modules/frontend/src/js/Shared/MainMenu.vue
+++ modules/frontend/src/js/Shared/MainMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div class="mb-4">
-            <inertia-link v-for="(item, i) in $page.props.mainNav" class="flex items-center group py-3" :href="item.url" :key="i">
+        <div class="md:mb-4 flex md:block">
+            <inertia-link v-for="(item, i) in $page.props.mainNav" class="flex items-center group py-3 px-4 md:px-0" :href="item.url" :key="i">
                 <div :class="isUrl(item.url) ? 'text-white' : 'text-brand-200 group-hover:text-white'">{{ item.label }}</div>
             </inertia-link>
         </div>
