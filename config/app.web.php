7545922ff2ede77a181cb986da18d7d2cdf604cd
wsydn <wsydney@web.de>
2022-06-29 10:44:02 +0200

    Updates

--- config/app.web.php
+++ config/app.web.php
@@ -2,23 +2,13 @@
 
 use config\Env;
 use wsydney76\inertia\web\Request;
-use wsydney76\inertia\web\Response as WebResponse;
+
 
 return [
     'components' => [
 
-        // Uncomment when running Craft in headless mode
-        // Don't return HTML responses as JSON
-
-        /* response' => function() {
-             $config = [
-                 'class' => WebResponse::class,
-             ];
-             return Craft::createObject($config);
-         },*/
-
         'request' => [
-            'class' => Request::class,
+                'class' => Request::class,
             'cookieValidationKey' => Env::SECURITY_KEY
         ]
 
